3ea08920476e4627182a2c8dd12965f7c325fe30
ThomasKeller <tkdetrial@gmail.com>
2023-11-30 23:10:40 +0100

    Migrate Kostal Service

---
+++ src/HA.Kostal.Service/AppSettings.cs
@@ -0,0 +1,187 @@
+﻿namespace HA.Kostal.Service;
+
+public class AppSettings
+{
+    private const string c_StoreFileName = "kostal.db";
+    private const string c_StoreFolderName = "store";
+    private const string c_SettingsFileName = "appsettings.json";
+    private const string c_SettingsFolderName = "settings";
+    private readonly string _workDirectory;
+
+    public string SettingsFilePath { get; set; } = Path.Combine(c_SettingsFolderName, c_SettingsFileName);
+
+    public string MeasurmentStoreFilePath { get; set; } = Path.Combine(c_StoreFolderName, c_StoreFileName);
+
+    public string KostalUrl { get; set; } = "http://192.168.111.4/";
+
+    public string KostalUser { get; set; } = "pvserver";
+
+    public string? KostalPassword { get; set; }
+
+    public bool KostalStopDuringSunset { get; set; } = true;
+
+    public string InfluxUrl { get; set; } = "http://192.168.111.237:8086/";
+
+    public string InfluxOrg { get; set; } = "Keller";
+
+    public string InfluxBucket { get; set; } = "ha_test";
+
+    public string? InfluxToken { get; set; }
+
+    public string MqttHost { get; set; } = "localhost";
+
+    public int MqttPort { get; set; } = 1883;
+
+    public AppSettings(string workDir = "")
+    {
+        _workDirectory = workDir;
+        var settingsPath = CreateDirectory(_workDirectory, c_SettingsFolderName);
+        var storePath = CreateDirectory(_workDirectory, c_StoreFolderName);
+
+        SettingsFilePath = Path.Combine(settingsPath, c_SettingsFileName);
+        MeasurmentStoreFilePath = Path.Combine(storePath, c_StoreFileName);
+    }
+
+    public void Read()
+    {
+        var envConfig = Environment.GetEnvironmentVariable("ENV_CONFIG", EnvironmentVariableTarget.Process)?.ToUpperInvariant();
+        if (envConfig != null && (envConfig == "1" || envConfig == "TRUE"))
+        {
+            Console.WriteLine("Read configuration from environment variables.");
+            ReadEnvironmentParameters();
+        }
+        else if (!File.Exists(SettingsFilePath))
+        {
+            var settings = ResourceSettings.ResourceManager.GetString("settings");
+            File.WriteAllText(SettingsFilePath, settings);
+            Console.WriteLine("Write configuration file '{0}'.", SettingsFilePath);
+            throw new Exception("Please configure service");
+        }
+        else
+        {
+            Console.WriteLine("Read configuration from file: {0}", SettingsFilePath);
+            ReadAppConfigFile(SettingsFilePath);
+        }
+        ConsolidateAppSettings();
+    }
+
+    private string CreateDirectory(string workDir, string folderName)
+    {
+        var folderPath = folderName;
+        if (workDir != "")
+        {
+            folderPath = Path.Combine(workDir, folderName);
+        }
+        if (!Directory.Exists(folderPath))
+        {
+            var info = Directory.CreateDirectory(folderPath);
+            Console.WriteLine("Create directory. {0}", info.FullName);
+        }
+        return folderPath;
+    }
+
+    private void ReadAppConfigFile(string appConfigFilePath)
+    {
+        var configurationBuilder = new ConfigurationBuilder();
+        IConfiguration conf = configurationBuilder
+            .AddJsonFile(appConfigFilePath)
+            .AddEnvironmentVariables()
+            .Build();
+
+        var sectionKostal = conf.GetRequiredSection("kostal");
+        var kostalUrl = sectionKostal.GetValue<string>("url");
+        if (kostalUrl != null)
+            KostalUrl = kostalUrl;
+        var kostalUser = sectionKostal.GetValue<string>("user");
+        if (kostalUser != null)
+            KostalUser = kostalUser;
+        var kostalPassword = sectionKostal.GetValue<string>("password");
+        if (kostalPassword != null)
+            KostalPassword = kostalPassword;
+        KostalStopDuringSunset = sectionKostal.GetValue("stopDuringSunset", true);
+
+        var sectionInflux = conf.GetRequiredSection("influxDB");
+        var influxUrl = sectionInflux.GetValue<string>("url");
+        if (influxUrl != null)
+            InfluxUrl = influxUrl;
+        var org = sectionInflux.GetValue<string>("org");
+        if (org != null)
+            InfluxOrg = org;
+        var bucket = sectionInflux.GetValue<string>("bucket");
+        if (bucket != null)
+            InfluxBucket = bucket;
+        var token = sectionInflux.GetValue<string>("token");
+        if (token != null)
+            InfluxToken = token;
+
+        var sectionMqtt = conf.GetRequiredSection("mqtt");
+        MqttHost = sectionMqtt.GetValue<string>("host") ?? "localhost";
+        MqttPort = sectionMqtt.GetValue("port", 1883);
+    }
+
+    private void ConsolidateAppSettings()
+    {
+        Console.WriteLine("Kostal-Url:         {0}", KostalUrl);
+        Console.WriteLine("Kostal-User:        {0}", KostalUser);
+        Console.WriteLine("Kostal-Password:    {0}", KostalPassword != null);
+        Console.WriteLine("Kostal-Sunset-Read: {0}", KostalStopDuringSunset);
+        Console.WriteLine();
+        Console.WriteLine("Influx-URL:         {0}", InfluxUrl);
+        Console.WriteLine("Influx-Org:         {0}", InfluxOrg);
+        Console.WriteLine("Influx-Bucket:      {0}", InfluxBucket);
+        Console.WriteLine("Influx-Token:       {0}", InfluxToken != null);
+        Console.WriteLine();
+        Console.WriteLine("\"MQTT-Host:        {0}", MqttHost);
+        Console.WriteLine("\"MQTT-Port:        {0}", MqttPort);
+
+        if (InfluxToken == null || KostalPassword == null)
+        {
+            Console.WriteLine("Please provider environmet variables or config file:");
+            Console.WriteLine("KOSTAL_URL      | http://192.168.111.4/");
+            Console.WriteLine("KOSTAL_USER     | pvserver");
+            Console.WriteLine("KOSTAL_PASSWORD | password");
+            Console.WriteLine();
+            Console.WriteLine("INFLUX_URL      | http://192.168.111.237:8086/");
+            Console.WriteLine("INFLUX_ORG      | Keller");
+            Console.WriteLine("INFLUX_BUCKET   | ha_test");
+            Console.WriteLine("INFLUX_TOKEN    | token");
+            Console.WriteLine();
+            if (InfluxToken != null)
+                throw new ArgumentNullException("INFLUX_TOKEN");
+            else
+                throw new ArgumentNullException("KOSTAL_PASSWORD");
+        }
+    }
+
+    private void ReadEnvironmentParameters()
+    {
+        var environmentVariableTarget = EnvironmentVariableTarget.Process;
+        var envKostalUrl = Environment.GetEnvironmentVariable("KOSTAL_URL", environmentVariableTarget);
+        if (envKostalUrl != null)
+            KostalUrl = envKostalUrl;
+        var envKostalUser = Environment.GetEnvironmentVariable("KOSTAL_USER", environmentVariableTarget);
+        if (envKostalUser != null)
+            KostalUser = envKostalUser;
+        var envKostalPassword = Environment.GetEnvironmentVariable("KOSTAL_PASSWORD", environmentVariableTarget);
+        if (envKostalPassword != null)
+            KostalPassword = envKostalPassword;
+        var envInfluxUrl = Environment.GetEnvironmentVariable("INFLUX_URL", environmentVariableTarget);
+        if (envInfluxUrl != null)
+            InfluxUrl = envInfluxUrl;
+        var envInfluxOrg = Environment.GetEnvironmentVariable("INFLUX_ORG", environmentVariableTarget);
+        if (envInfluxOrg != null)
+            InfluxOrg = envInfluxOrg;
+        var envInfluxBucket = Environment.GetEnvironmentVariable("INFLUX_BUCKET", environmentVariableTarget);
+        if (envInfluxBucket != null)
+            InfluxBucket = envInfluxBucket;
+        var envInfluxToken = Environment.GetEnvironmentVariable("INFLUX_TOKEN", environmentVariableTarget);
+        if (envInfluxToken != null)
+            InfluxToken = envInfluxToken;
+        MqttHost = Environment.GetEnvironmentVariable("MQTT_HOST", environmentVariableTarget) ?? "192.168.111.50";
+        var envMqttPort = Environment.GetEnvironmentVariable("MQTT_PORT", environmentVariableTarget) ?? "1883";
+        if (int.TryParse(envMqttPort, out int value))
+        {
+            MqttPort = value;
+        }
+    }
+}
---
+++ src/HA.Kostal.Service/Components.cs
@@ -0,0 +1,121 @@
+﻿using HA;
+using HA.Influx;
+using HA.Mqtt;
+using HA.Store;
+using System.Text;
+
+namespace HA.Kostal.Service
+{
+    public class Components
+    {
+        private readonly ILoggerFactory _loggerFactory;
+        private readonly ILogger _logger;
+        private ConsoleObserver? _consoleObserver;
+
+        public Components(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory ?? throw new ArgumentNullException(nameof(loggerFactory));
+            _logger = _loggerFactory.CreateLogger<Components>();
+        }
+
+        public KostalObservable? MeasurmentObservable { get; private set; }
+
+        public InfluxResilientStore? InfluxResilientStore { get; set; }
+
+        public MeasurementObserver? InfluxMeasurementObserver { get; set; }
+
+        public MeasurementObserver? MqttMeasurementObserver { get; set; }
+
+        public MqttPublisher? HealthMqttPublisher { get; private set; }
+
+        public void Init(string workDir = "")
+        {
+            var appSettings = new AppSettings(workDir);
+            appSettings.Read();
+
+            InitIKostalComponent(appSettings);
+            InitInfluxComponent(appSettings);
+            InitMqttComponent(appSettings);
+        }
+
+        public void EnableConsoleObserver()
+        {
+            _consoleObserver = new ConsoleObserver();
+            MeasurmentObservable?.Subscribe(_consoleObserver);
+        }
+
+        public string CurrentStatus()
+        {
+            var sb = new StringBuilder();
+            if (MeasurmentObservable != null && InfluxResilientStore != null && InfluxMeasurementObserver != null)
+            {
+                // initialized
+                sb.Append($"Last Event sent: {(DateTime.Now - MeasurmentObservable.LastMeasurementSentAt).TotalSeconds} s ");
+                sb.Append($"[Measurement] Last Proccessed: {(DateTime.Now - InfluxMeasurementObserver.LastMeasurementProccessed).TotalSeconds} s ");
+                sb.Append($"[Influx] Queue Count: {InfluxResilientStore.QueueCount} Error Count: {InfluxResilientStore.InfluxErrorCount} ");
+            }
+            else
+            {
+                sb.AppendLine("Status: Not Ready, components un-initialized");
+            }
+            return sb.ToString();
+        }
+
+        public IDictionary<string, string> CurrentComponentsStatus()
+        {
+            var status = new Dictionary<string, string>();
+            if (MeasurmentObservable != null && InfluxResilientStore != null && InfluxMeasurementObserver != null)
+            {
+                var root = "health/kostalservice/";
+                status.Add($"{root}lastHeartBeat", DateTime.Now.ToString("o"));
+                status.Add($"{root}observable/lastMeasurementSec",
+                    (DateTime.Now - MeasurmentObservable.LastMeasurementSentAt).TotalSeconds.ToString("#.000"));
+                status.Add($"{root}influx/lastMeasurementStoredSec",
+                    (DateTime.Now - InfluxMeasurementObserver.LastMeasurementProccessed).TotalSeconds.ToString("#.000"));
+                status.Add($"{root}influx/queueCount", InfluxResilientStore.QueueCount.ToString());
+                status.Add($"{root}influx/totalErrorCount", InfluxResilientStore.InfluxErrorCount.ToString());
+            }
+            return status;
+        }
+
+        private void InitIKostalComponent(AppSettings appSettings)
+        {
+            MeasurmentObservable = new KostalObservable(
+                _loggerFactory.CreateLogger<KostalObservable>(),
+                new KostalClient(appSettings.KostalUrl, appSettings.KostalUser, appSettings.KostalPassword));
+            MeasurmentObservable.StopDuringSunset = appSettings.KostalStopDuringSunset;// envKostalStopDuringNight == "true";
+        }
+
+        private void InitInfluxComponent(AppSettings appSettings)
+        {
+            InfluxResilientStore = new InfluxResilientStore(
+                _loggerFactory.CreateLogger<InfluxResilientStore>(),
+                new InfluxSimpleStore(
+                    appSettings.InfluxUrl,
+                    appSettings.InfluxBucket,
+                    appSettings.InfluxOrg,
+                    appSettings.InfluxToken),
+                new MeasurementStore(appSettings.MeasurmentStoreFilePath));
+            InfluxMeasurementObserver = new MeasurementObserver(
+                    _loggerFactory.CreateLogger<MeasurementObserver>(),
+                    InfluxResilientStore);
+            if (MeasurmentObservable != null)
+                InfluxMeasurementObserver.Subscribe(MeasurmentObservable);
+        }
+
+        private void InitMqttComponent(AppSettings appSettings)
+        {
+            var mqttPublisher = new MqttPublisher(
+                _loggerFactory.CreateLogger<MqttPublisher>(),
+                appSettings.MqttHost,
+                appSettings.MqttPort,
+                "ha.kostal.service");
+            HealthMqttPublisher = mqttPublisher;
+            MqttMeasurementObserver = new MeasurementObserver(
+                   _loggerFactory.CreateLogger<MeasurementObserver>(),
+                   mqttPublisher);
+            if (MeasurmentObservable != null)
+                MqttMeasurementObserver.Subscribe(MeasurmentObservable);
+        }
+    }
+}
